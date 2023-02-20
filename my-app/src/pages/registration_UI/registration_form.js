6247d0dc279336b8748bd3bf8d6e4d904570c243
Michael <AstroMike101@gmail.com>
2023-02-19 19:33:42 -0500

    more changes

--- my-app/src/pages/registration_UI/registration_form.js
+++ my-app/src/pages/registration_UI/registration_form.js
@@ -4,31 +4,66 @@ function RegistrationForm() {
     return (
         <div className="form">
             <h1 class='text'>Register an Account </h1>
+
             <div className="form-body">
                 <div className="username">
-                    <label className="form__label text" for="Name">Name </label>
-                    <input class="form__input" type="text" id="Name" placeholder="Name" />
+                    <input class="form__input" type="text all" id="Name" placeholder="Name" />
                 </div>
                
                 <div className="email">
-                    <label className="form__label text " for="email">Email </label>
-                    <input type="email" id="email" className="form__input" placeholder="Email" />
+                    <input type="email" id="email" className="form__input all" placeholder="Email" />
                 </div>
                 <div className="password">
-                    <label className="form__label text " for="password">Password </label>
-                    <input className="form__input" type="password" id="password" placeholder="Password" />
+                   
+                    <input className="form__input all" type="password" id="password" placeholder="Password" />
                 </div>
                 <div className="confirm-password">
-                    <label className="form__label text " for="confirmPassword">Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password" />
+                    <input className="form__input all" type="password" id="confirmPassword" placeholder="Confirm Password" />
                 </div>
 
              
                 <div className="phoneNumber">
-                    <label className="form__label text" for="phoneNumber">Phone Number </label>
-                    <input className="form__input" type="phoneNumber" id="phoneNumber" placeholder="Phone Number" />
+                    <input className="form__input all" type="phoneNumber" id="phoneNumber" placeholder="Phone Number" />
                 </div>
                 <h1 class='text'>Payment Information</h1>
+           
+
+                    <div className="cardType">
+                        <input className="form__input all" type="cardType" id="cardType" placeholder="Card Type" />
+                    </div>
+
+                    <div className="cardNumber">
+                        <input className="form__input all" type="cardNumber" id="cardNumber" placeholder="Card #" />
+                    </div>
+
+
+                    <div className="experationDate">
+                        <input className="form__input all" type="experationDate" id="experationDate" placeholder="Expiration Date" />
+                    </div>
+
+
+            
+                <h1 class='text'>Billing Information</h1>
+
+                <div className="username">
+                    <input class="form__input" type="text all" id="Name" placeholder="Name" />
+                </div>
+
+                <div className="address">
+                    <input className="form__input all" type="address" id="address" placeholder="Address" />
+                </div>
+
+
+                <div className="zipCode">
+                    <input className="form__input all" type="zipCode" id="zipCode" placeholder="Zip Code" />
+                </div>
+
+
+
+
+
+
+
             </div>
             <div class="footer">
                 <button type="submit" class="btn">Register</button>
