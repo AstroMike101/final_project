56e12cf3647a0ddad8db07f0dc9d637db4770844
AstroMike101 <65747601+AstroMike101@users.noreply.github.com>
2023-02-21 15:03:38 -0500

    final version.... hopefully

--- my-app/src/pages/adminPortal/adminPortal.js
+++ my-app/src/pages/adminPortal/adminPortal.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './adminPortal.css'
 import { Button, Space } from 'antd';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
+import promotionAdd from '../../PromotionAdd';
+import ManageMovies from '../../ManageMovies';
+
 function AdminPortal() {
     return (
+        
         <div className="container">
             <header className="header text">
                 <h1 classname='text'>Admin Portal</h1>
@@ -11,8 +16,8 @@ function AdminPortal() {
                 <div className="left-section">
                     <h2 className="text">Manage Movies</h2>
                     <div className="buttons">
-                        <button>Add movie</button>
-                        <button>Remove movie</button>
+                        {/* <button>Add/Remove Movies</button> */}
+                        <NavLink to="/ManageMovies" ><button type="primary">Add & Remove Movies </button></NavLink>
                         <button>Edit Movie Scheduling</button>
                     </div>
                 </div>
@@ -20,7 +25,9 @@ function AdminPortal() {
                 <div className="right-section">
                     <h2 className="text">Manage Promotions</h2>
                     <div className="buttons">
-                        <button>Make new promotion</button>
+
+                        <NavLink to="/promotionAdd" ><button type="primary">Make new Promotion </button></NavLink>
+                        
                         <button>Remove existing promotions</button>
                         <button>View current promotions</button>
                     </div>
