dd60da01d5b9d3ac4d9ce6f913adee9695267aaa
AstroMike101 <65747601+AstroMike101@users.noreply.github.com>
2023-02-20 19:45:25 -0500

    login done

--- my-app/src/pages/login_UI/login_UI.js
+++ my-app/src/pages/login_UI/login_UI.js
@@ -2,29 +2,38 @@ import React, { useState } from 'react';
 import './login.css'
 
 
-
 function login() {
     return (
         <div className="form">
-            <form>
-                <div className="input-container">
-                    <label>Username </label>
-                    <input type="text" name="uname" required />
-                    {renderErrorMessage("uname")}
+            <h1 class='text'>Sign in</h1>
+
+            <div className="form-body">
+                <div className="username">
+                    <input class="form__input" type="text all" id="username" placeholder="Username" />
                 </div>
-                <div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="pass" required />
-                    {renderErrorMessage("pass")}
+
+                <div className="password">
+                    <input type="email" id="password" className="form__input all" placeholder="Password" />
                 </div>
-                <div className="button-container">
-                    <input type="submit" />
-                </div>
-            </form>
+                <p class='text'>Dont have an account?  Sign up <a href='#'>here</a></p>
+
+
+
+            </div>
+            <div class="footer">
+                <button type="submit" class="btn">Sign in</button>
+            </div>
         </div>
     );
-      
+
 
 }
+
+
+
+
+
+
+
 
 export default login;
