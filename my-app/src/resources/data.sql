2bcb151d5e183a8accc86c07f729b47c03663944
Brent Voyles <btv88975@uga.edu>
2023-03-19 10:50:42 -0400

    Added MySQL db to resources

---
+++ my-app/src/resources/data.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 19, 2023 at 03:32 PM
+-- Server version: 8.0.32
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ebookingdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+CREATE TABLE `booking` (
+  `bookingID` int NOT NULL,
+  `userID` int NOT NULL,
+  `showID` int NOT NULL,
+  `paymentID` int NOT NULL,
+  `total` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE `movie` (
+  `movieID` int NOT NULL,
+  `movieTitle` varchar(255) NOT NULL,
+  `trailerLink` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment` (
+  `paymentID` int NOT NULL,
+  `userID` int NOT NULL,
+  `cardNumber` varchar(255) NOT NULL,
+  `cardType` varchar(255) NOT NULL,
+  `expDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `show`
+--
+
+DROP TABLE IF EXISTS `show`;
+CREATE TABLE `show` (
+  `showID` int NOT NULL,
+  `movieID` int NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `roomID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='	';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `showroom`
+--
+
+DROP TABLE IF EXISTS `showroom`;
+CREATE TABLE `showroom` (
+  `showID` int NOT NULL,
+  `roomName` varchar(255) NOT NULL,
+  `numSeats` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE `ticket` (
+  `ticketID` int NOT NULL,
+  `bookingID` int NOT NULL,
+  `ticketType` varchar(255) NOT NULL,
+  `seatNumber` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickettype`
+--
+
+DROP TABLE IF EXISTS `tickettype`;
+CREATE TABLE `tickettype` (
+  `ttypeID` int NOT NULL,
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `userID` int NOT NULL,
+  `userFirstName` varchar(255) NOT NULL,
+  `userLastName` varchar(255) NOT NULL,
+  `userType` int NOT NULL DEFAULT '1',
+  `userEmail` varchar(255) NOT NULL,
+  `userPW` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Need to find which datatype is best for secure password. Default value set for userType = 1. Not verified yet.';
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `userFirstName`, `userLastName`, `userType`, `userEmail`, `userPW`) VALUES
+(1, 'Brent', 'Voyles', 1, 'Brent.Voyles3@gmail.com', '1234567!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertype`
+--
+
+DROP TABLE IF EXISTS `usertype`;
+CREATE TABLE `usertype` (
+  `typeID` int NOT NULL,
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`bookingID`),
+  ADD UNIQUE KEY `bookingID_UNIQUE` (`bookingID`),
+  ADD KEY `userID_idx` (`userID`),
+  ADD KEY `pID_idx` (`paymentID`),
+  ADD KEY `sID_idx` (`showID`);
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`movieID`),
+  ADD UNIQUE KEY `movieID_UNIQUE` (`movieID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`paymentID`),
+  ADD UNIQUE KEY `ID_UNIQUE` (`paymentID`),
+  ADD KEY `userID_idx` (`userID`);
+
+--
+-- Indexes for table `show`
+--
+ALTER TABLE `show`
+  ADD PRIMARY KEY (`showID`),
+  ADD UNIQUE KEY `showID_UNIQUE` (`showID`),
+  ADD UNIQUE KEY `movieID_UNIQUE` (`movieID`),
+  ADD UNIQUE KEY `roomID_UNIQUE` (`roomID`),
+  ADD UNIQUE KEY `time_UNIQUE` (`time`),
+  ADD UNIQUE KEY `data_UNIQUE` (`date`);
+
+--
+-- Indexes for table `showroom`
+--
+ALTER TABLE `showroom`
+  ADD PRIMARY KEY (`showID`),
+  ADD UNIQUE KEY `id_UNIQUE` (`showID`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticketID`),
+  ADD UNIQUE KEY `ticketID_UNIQUE` (`ticketID`),
+  ADD KEY `bID_idx` (`bookingID`);
+
+--
+-- Indexes for table `tickettype`
+--
+ALTER TABLE `tickettype`
+  ADD PRIMARY KEY (`ttypeID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `userEmail_UNIQUE` (`userEmail`),
+  ADD UNIQUE KEY `UserID_UNIQUE` (`userID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `pID` FOREIGN KEY (`paymentID`) REFERENCES `payment` (`paymentID`),
+  ADD CONSTRAINT `sID` FOREIGN KEY (`showID`) REFERENCES `show` (`showID`),
+  ADD CONSTRAINT `uID` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Constraints for table `show`
+--
+ALTER TABLE `show`
+  ADD CONSTRAINT `mID` FOREIGN KEY (`movieID`) REFERENCES `movie` (`movieID`),
+  ADD CONSTRAINT `rID` FOREIGN KEY (`showID`) REFERENCES `showroom` (`showID`);
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `bID` FOREIGN KEY (`bookingID`) REFERENCES `booking` (`bookingID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
