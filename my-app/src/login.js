b73ab92c0974b93deeb410eac21007e8720b7317
kavin-cmd <kpk1611@gmail.com>
2023-03-22 15:31:51 -0400

    Delete my-app directory

--- my-app/src/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
-
-// create an Express application
-const app = express();
-
-// enable parsing of JSON request bodies
-app.use(express.json());
